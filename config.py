9e8f858c0be5c8f4ab4bea3e370c104385cac7b1
DopeBotz <87130137+DopeBotz@users.noreply.github.com>
2021-08-18 19:54:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config(object):
-    token = os.environ.get("TOKEN", None)
-    api_id = os.environ.get("API_ID", None)
-    api_hash = os.environ.get("API_HASH", None)
-    log_id = int(os.environ.get("LOG_ID", 0))
+    token = "1921635851:AAHgKoe5MV_jmnuRi6VIw-4yj5Zzsig4o2E"
+    api_id = "5689646"
+    api_hash = "895de5ae804308803c19814afabb0de7"
+    log_id = "-1001177104541"
     start_msg = os.environ.get("START_MSG", None)
-    ownerID = os.environ.get("OWNER_ID", [])
+    ownerID = "1808767615"
     if ownerID:
         try:
             ownerID = [int(ID) for ID in ownerID.split("|")]
